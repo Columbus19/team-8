42acc2ec61b4c6fa7cdd1a854ebeb1da6229a1d6
Jerry Tan <jerry88tan@gmail.com>
2019-10-19 05:24:39 -0400

    add files

---
+++ model.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    ssn = db.Column(db.String(120), unique=True, nullable=False)
+    phone_number = db.Column(db.String(120), unique=True, nullable=False)
+    monthly_income = db.Column(db.String(120), unique=True, nullable=False)
+    value_of_assets = db.Column(db.String(120), unique=True, nullable=False)
+    total_debt = db.Column(db.Integer, unique=True, nullable=False)
+    debt_dist1 = db.Column(db.Integer, unique=True, nullable=False)
+    debt_dist2 = db.Column(db.Integer, unique=True, nullable=False)
+    debt_dist3 = db.Column(db.Integer, unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '%r' % self.fullname
+
+db.create_all()
+db.session.commit()
