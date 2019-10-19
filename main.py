ece8450e479efcfe3619137e24add958e55237bf
Ellen Tsao <etsao@umich.edu>
2019-10-19 02:25:07 -0400

    backend changes

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ import pdb
 @app.route("/")
 def home():
     joe = User(fullname='Joseph Nastasi', email='joe@jp.org', ssn='123-45-6789',phone_number='8106236861', monthly_income='1,000,000', value_of_assets='1,000,000', total_debt=200000,debt_dist1=10,debt_dist2=20,debt_dist3=70)
-    cajevamiel = User(fullname='Caj Evamiel', email='caj@jp.org', ssn='987-54-321',phone_number='12345678', monthly_income='2,000,000', value_of_assets='2,000,000',total_debt=300000,debt_dist1=50,debt_dist2=30,debt_dist3=20)
+    average = User(fullname='AVERAGE', email='avg@jp.org', ssn='987-54-321',phone_number='12345678', monthly_income='2,000,000', value_of_assets='2,000,000',total_debt=300000,debt_dist1=50,debt_dist2=30,debt_dist3=20)
     db.session.add(joe)
-    db.session.add(cajevamiel)
+    db.session.add(average)
     db.session.commit()
     # name = User.query.filter_by(fullname='Joseph Nastasi').first()
-    dynamic_bar = User.query.filter_by(fullname='Joseph Nastasi').first()
+    joe_data = User.query.filter_by(fullname='Joseph Nastasi').first()
     
-    val1 = dynamic_bar.debt_dist1
-    val2 = dynamic_bar.debt_dist2
-    val3 = dynamic_bar.debt_dist3
+    bar = joe_data.total_debt
+    val1 = joe_data.debt_dist1
+    val2 = joe_data.debt_dist2
+    val3 = joe_data.debt_dist3
 
-    return render_template("chart.html", dynamic_bar=dynamic_bar, val1=val1, val2=val2, val3=val3)
+    avg_data = User.query.filter_by(fullname='AVERAGE').first()
+
+    avg1 = avg_data.debt_dist1
+    avg2 = avg_data.debt_dist2
+    avg3 = avg_data.debt_dist3
+
+    return render_template("chart.html", joe_data=joe_data, val1=val1, val2=val2, val3=val3, bar=bar, avg1=avg1, avg2=avg2, avg3=avg3)
     
 @app.route("/profile")
 def profile():
