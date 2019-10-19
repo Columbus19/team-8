1aabdf9ee0054e28b5b8b83c1dd759fbf2775ed3
Jerry Tan <jerry88tan@gmail.com>
2019-10-18 22:49:39 -0400

    add d3 visualizations

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template      
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
+
+
 app = Flask(__name__)
+nav = Nav(app)
+
+nav.register_element('my_navBar', Navbar(
+	'Nav',
+View('Home Page', 'home'),
+View('Profile', 'profile'))
+)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
-
+def home():
+    return render_template("chart.html")
+    
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+    
 if __name__ == "__main__":
-	app.run()
+    app.run(debug=True)
