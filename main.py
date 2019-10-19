9f222f5fe24ef31200d92838a8d61781c6e22ca7
Ellen Tsao <etsao@umich.edu>
2019-10-18 23:39:36 -0400

    database

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from model import User, db, app
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    joe = User(fullname='Joseph Nastasi', email='joe@jp.org', ssn='123-45-6789',phone_number='8106236861', monthly_income='1,000,000', value_of_assets='1,000,000')
+    cajevamiel = User(fullname='Caj Evamiel', email='caj@jp.org', ssn='987-54-321',phone_number='12345678', monthly_income='2,000,000', value_of_assets='2,000,000')
+    db.session.add(joe)
+    db.session.add(cajevamiel)
+    db.session.commit()
+    result = User.query.filter_by(fullname='Joseph Nastasi').first()
+    print(result)
+    return render_template('index.html')
 
-if __name__ == "__main__":
-	app.run()
+if __name__ == '__main__':
+    app.run()
