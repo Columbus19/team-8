2384702187c2a29ef5d5b44d2f12494e2201fc44
Jerry Tan <jerry88tan@gmail.com>
2019-10-18 22:53:21 -0400

    remove nav

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from flask import Flask, render_template      
-from flask_nav import Nav
-from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
+
 
 
 app = Flask(__name__)
-nav = Nav(app)
 
-nav.register_element('my_navBar', Navbar(
-	'Nav',
-View('Home Page', 'home'),
-View('Profile', 'profile'))
-)
 
 @app.route("/")
 def home():
